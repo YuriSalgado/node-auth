66790cae52644887b57ad22f9bcbe88324e99fb8
unknown <yuri_salgadinh@hotmail.com>
2021-05-18 20:59:04 -0300

    mongoose + models

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://localhost/node_auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('connect to the database')
+})
+
 app = express()
 
 app.get('/', (req, res) => {
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const useSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model('User', useSchema)
