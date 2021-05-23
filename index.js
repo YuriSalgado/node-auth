78c47a7699ee56ec0b79e64871409f9fd2ea3459
unknown <yuri_salgadinh@hotmail.com>
2021-05-22 21:51:40 -0300

    routers register + login with set jwt in cookies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
 
-mongoose.connect('mongodb://localhost/node_auth', {
+mongoose.connect('mongodb://localhost:27017/node_auth', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 router.post('/register', async (req, res) => {    
@@ -11,8 +12,39 @@ router.post('/register', async (req, res) => {
         email: req.body.email,
         password: hashedPassword
     })
+
+    const result = await user.save()
     
-    res.send(await user.save())
+    const {password, ...data} = await result.toJSON()
+
+    res.send(data)
+})
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if (!user) {
+        return res.status(404).send({
+            message: 'user not found'
+        })
+    }
+
+    if (!await bcrypt.compare(req.body.password, user.password)) {
+        return res.status(400).send({
+            message: 'invalid credentials'
+        })
+    }
+
+    const token = jwt.sign({ id: user._id}, "secret")
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000 // 1d 
+    })
+
+    res.send({
+        message: 'success'
+    })
 })
 
 module.exports = router;
