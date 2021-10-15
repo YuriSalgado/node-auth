b23bb2b36845babf73d57bdccbe0af1efc3291ce
yuri salgado <yuri_salgadinho@hotmail.com>
2021-10-15 17:44:02 -0300

    add react address

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:4200']
+    origin: ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:4200', 'http://localhost:3000']
 }))
 
 app.use(express.json())
